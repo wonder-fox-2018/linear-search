6eaf883dd7ae942de476252e71de192f484d68bb
Hilarion18 <hilarion_cross@hotmail.com>
2018-09-04 15:52:54 +0700

    linear 2

--- linear_search.js
+++ linear_search.js
@@ -1,5 +1,5 @@
 'use strict'
-/*
+
 // Release 0
 console.log("Linear Search")
 function linearSearch(target, values) {
@@ -15,34 +15,34 @@ function linearSearch(target, values) {
   return result
 }
 
-// let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
-console.log(linearSearch(18, [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ]));
+console.log(linearSearch(18, random_numbers));
 // 2
-console.log(linearSearch(9, [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ]));
+console.log(linearSearch(9, random_numbers));
 // -1
-*/
+
 // Release 1
-console.log("Global Linear Search")
-function globalLinearSearch(target, values) {
-  let result = []
-  for ( let i = 0; i < values.length; i++) {
-    if ( target === values[i]) {
-      result.push(i)
-    }
-  }
-  return result
-  if ( result === []) {
-    return 'tidak ada'
-  }
-}
+// console.log("Global Linear Search")
+// function globalLinearSearch(target, values) {
+//   let result = []
+//   for ( let i = 0; i < values.length; i++) {
+//     if ( target === values[i]) {
+//       result.push(i)
+//     }
+//   }
+//   return result
+//   if ( result === []) {
+//     return 'tidak ada'
+//   }
+// }
 
 // let banana_arr = "banana".split("");
-// ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", ["b", "a", "n", "a", "n", "a"]));
-// [ 1, 3, 5 ]
+// // ["b", "a", "n", "a", "n", "a"]
+// console.log(globalLinearSearch("a", banana_arr));
+// // [ 1, 3, 5 ]
 
-// module.exports = {
-//   linearSearch,
-//   globalLinearSearch
-// }
+// // module.exports = {
+// //   linearSearch,
+// //   globalLinearSearch
+// // }
