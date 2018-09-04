be6d48898b3b50fb94db410509378b5f87358217
danyismail <dany_arie@yahoo.com>
2018-09-04 21:02:21 +0700

    linear search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,12 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (let i = 0; i < random_numbers.length; i++) {
+    if (random_numbers[i] === target) {
+      return i
+    }
+  } 
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +21,13 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var result = []
+  for (let i = 0; i < banana_arr.length; i++) {
+    if (banana_arr[i] === target) {
+      result.push(i)
+    }
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
