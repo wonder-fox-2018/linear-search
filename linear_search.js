4a7f9c831cbc0d7536504c43e605ae9ca0898267
hadinw <hadiykk99@gmail.com>
2018-09-04 21:38:49 +0700

    commit linear

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,17 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  var count = 0;
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      count++
+    }
+  }
+  if (count > 0) {
+    return count
+  } else{
+    return -1
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +26,13 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var result = []
+  for (var i = 0; i < values.length; i++) {
+    if (target === values[i]) {
+      result.push(i)
+    }
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
