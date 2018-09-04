6549806944db4e871a9abf13ceaf3a8064ef35a5
Your Name <hokandre@mhs.mdp.ac.id>
2018-09-04 14:38:11 +0700

    linear search

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(var i=0;i<values.length;i++){
+    if(target===values[i]){
+      return i
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +23,16 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var result=[]
+  for(var i=0;i<values.length;i++){
+    if(target===values[i]){
+      result.push(i)
+    }
+  }
+  if(result.length===0){
+    return -1
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
