9cd010d3084c4faac5f671f174877a0d70ee5f45
ajinfadhila <ajinfadhila@gmail.com>
2018-09-04 21:02:50 +0700

    up linear

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,14 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (var i = 0; i < values.length; i++) {
+    if (target === values[i]) {
+      var tmp = i
+      return tmp
+    }
+  }
+  return -1
+  // console.log(tmp)
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -13,18 +21,25 @@ console.log(linearSearch(18, random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
-// Release 1
-console.log("Global Linear Search")
+// // Release 1
+// console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var arr = []
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      arr.push(i)
+    }
+  }
+  return arr
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
-// [ 1, 3, 5 ]
-
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// // [ 1, 3, 5 ]
+//
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
